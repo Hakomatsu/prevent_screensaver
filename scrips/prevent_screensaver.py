f1ed51e3fa3b561ba14a64ebd689fcddd63fa9bb
Hakomatsu <hako.matsu.box@gmail.com>
2020-05-09 04:36:23 +0900

    Renamed.

--- scrips/prevent_screensaver.py
+++ scrips/prevent_screensaver.py
@@ -6,32 +6,44 @@ import time
 import numpy
 from idleTime import idle
 
-is_idle = False
+class PreventScreensaver:
+    def __init__(self, start_prevent):
+        self.is_idle = False
+        self.start = start_prevent
 
-def detect_idle(duration=180):
-    global is_idle
-    dur = idle.get_idle_duration()
-    time.sleep(duration)
-    # print(dur)
-    if dur >= duration-5:
-        # print("idle detected")
-        is_idle = True
-    else:
-        # print("not idle state")
-        is_idle = False
+    def detect_idle(self, duration=180):
+        dur = idle.get_idle_duration()
+        time.sleep(duration)
+        print(dur)
+        if dur >= duration-5:
+            print("idle detected")
+            self.is_idle = True
+        else:
+            print("not idle state")
+            self.is_idle = False
 
-def move_cursor():
-    global is_idle
-    pyautogui.move(-100,0,duration=1.0)
-    time.sleep(2.0)
-    pyautogui.move(100,0,duration=1.0)
-    is_idle = False
+    def move_cursor(self):
+        for i in range(10):
+            pyautogui.move(-100,0,duration=0.1)
+            pyautogui.move(100,0,duration=0.1)
+
+    def volume_control(self):
+        pyautogui.press("volumedown", presses=5)
+        time.sleep(1)
+        pyautogui.press("volumeup", presses=5)
+        time.sleep(1)
+
+    def surveillance_loop(self, duration=180):
+        try:
+            while self.start:
+                self.detect_idle(duration=duration)
+                if self.is_idle:
+                    self.move_cursor()
+                    self.volume_control()
+                    self.is_idle = False
+        except KeyboardInterrupt:
+            print("finish")
 
 if __name__ == "__main__":
-    try:
-        while True:
-            detect_idle()
-            if is_idle:
-                move_cursor()
-    except KeyboardInterrupt:
-        print("finish")
+    ps = PreventScreensaver(start_prevent=True)
+    ps.surveillance_loop(5)
