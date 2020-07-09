686297e1b4ba177b8e83128407c21b7a666d1c8b
Hakomatsu <hako.matsu.box@gmail.com>
2020-07-10 00:58:21 +0900

    Change loop control from single process to thread control.

--- scrips/gui.py
+++ scrips/gui.py
@@ -6,10 +6,12 @@ import os
 import sys
 import wx
 import wx.adv
+import threading
 import prevent_screensaver as ps
 
 TRAY_TOOLTIP = "Prevent Screensaver"
 TRAY_ICON = "icon.ico"
+pso = ps.PreventScreensaver()
 
 def create_menu_item(menu, label, func):
     item = wx.MenuItem(menu, -1, label)
@@ -26,13 +28,15 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
         self.frame = frame
         super(TaskBarIcon, self).__init__()
         self.set_icon(TRAY_ICON)
-        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DCLICK, self.on_prevent_screensaver)
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DCLICK, self.on_start)
+        self.Bind(wx.adv.EVT_TASKBAR_RIGHT_DCLICK, self.on_stop)
+        self.condition = False
     
     def CreatePopupMenu(self):
         menu = wx.Menu()
-        create_menu_item(menu, "Start", self.on_prevent_screensaver)
-        # menu.AppendSeparator()
-        # create_menu_item(menu, "Stop", self.prevent_screensaver)
+        create_menu_item(menu, "Start", self.on_start)
+        menu.AppendSeparator()
+        create_menu_item(menu, "Stop", self.on_stop)
         menu.AppendSeparator()
         create_menu_item(menu, "Exit", self.on_exit)
         return menu
@@ -44,15 +48,26 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
     def on_prevent_screensaver(self, event):
         prevent_screensaver()
     
+    def on_start(self, event):
+        global pso
+        self.condition = True
+        threading.Thread(target=pso.set_flag, args=(self.condition,)).start()
+    
+    def on_stop(self, event):
+        global pso
+        self.condition = False
+        threading.Thread(target=pso.set_flag, args=(self.condition,)).start()
+
     def on_exit(self, event):
-        print("exit")
+        self.on_stop(event)
         wx.CallAfter(self.Destroy)
         self.frame.Close()
+        print("Exit")
 
 class App(wx.App):
     def OnInit(self):
         frame = wx.Frame(None)
         self.SetTopWindow(frame)
         TaskBarIcon(frame)
-        print("launch App")
+        print("Launch App")
         return True
